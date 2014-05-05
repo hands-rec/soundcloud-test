0dbf5624503bc5ed817c16c0add5c51b9fc36c7d
hands-rec <hands.records@gmail.com>
2014-05-05 12:12:24 +0900

    users who favorited the track

---
+++ webroot/favoriters/js/soundcloud.js
@@ -0,0 +1,33 @@
+$(function(){
+  SC.initialize({
+  client_id: Config.client_id,
+  });
+
+  $('#btn_search').click(function(){
+    SC.get('/tracks', { q: $('#keyword').val(), limit: 100}, function(tracks) {
+      $('#search_result').empty();
+      if (0<tracks.length) {
+        $('#search_result').append('<ul>');
+        for (var i = 0; i < tracks.length; i++) {
+          $('#search_result').find('ul').append('<li>'+tracks[i].title+'&nbsp;<a href="#'+tracks[i].id+'">Show liked users</a></li>');
+        };
+        $('#search_result').append('</ul>');
+
+        $('#search_result a').click(function(){
+          var $aLink = $(this);
+          var track_id = $aLink.attr('href').replace('#','');
+          SC.get('/tracks/'+track_id+'/favoriters', function(users){
+            if (0<users.length) {
+              var $li = $aLink.parent();
+              $li.append('<ul>');
+              for (var i = 0; i < users.length; i++) {
+                $li.find('ul').append('<li>'+users[i].username+'</li>');
+              };
+              $li.append('</ul>')
+            }
+          });
+        });
+      }
+    });
+  });
+});
