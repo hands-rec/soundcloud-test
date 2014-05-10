493dce1eaaf33ead9db9eae441dc8aa5f9d98c6a
hands-rec <hands.records@gmail.com>
2014-05-10 17:58:09 +0900

    tweak

--- webroot/search_sound/js/soundcloud.js
+++ webroot/search_sound/js/soundcloud.js
@@ -1,20 +1,24 @@
 $(function(){
   SC.initialize({
-  client_id: Config.client_id,
+    client_id: Config.client_id,
   });
 
   $('#btn_search').click(function(e){
     e.preventDefault();
+
     SC.get('/tracks', { q: $('#keyword').val()}, function(tracks) {
       $('#search_result').empty();
-      if (0<tracks.length) {
+
+      if (0 < tracks.length) {
         $('#search_result').append('<ul>');
         for (var i = 0; i < tracks.length; i++) {
-          $('#search_result').find('ul').append('<li>'+tracks[i].title+'&nbsp;<a href="#'+tracks[i].id+'">Play</a></li>');
+          var li = '<li>'+tracks[i].title+'&nbsp;<a href="#'+tracks[i].id+'">Play</a></li>';
+          $('#search_result').find('ul').append(li);
         };
         $('#search_result').append('</ul>');
 
         $('#search_result a').click(function(){
+          e.preventDefault();
           track_no = $(this).attr('href').replace('#','');
           SC.stream('/tracks/'+track_no, function(sound){
             soundManager.stopAll();
