85bff8a86be4f247ad14bee4d97f9f0b6bd09ec7
hands-rec <hands.records@gmail.com>
2014-05-10 23:05:56 +0900

    add search order

--- webroot/search_sound/js/soundcloud.js
+++ webroot/search_sound/js/soundcloud.js
@@ -12,13 +12,18 @@ $(function(){
   $('#btn_search').click(function(e){
     e.preventDefault();
 
-    SC.get('/tracks', { q: $('#keyword').val()}, function(tracks) {
+    SC.get('/tracks', { q: $('#keyword').val(), limit:200, order:"created_at"}, function(tracks) {
       $('#search_result').empty();
 
       if (0 < tracks.length) {
         $('#search_result').append('<ul>');
         for (var i = 0; i < tracks.length; i++) {
-          var li = '<li>'+tracks[i].title+'&nbsp;<a href="#'+tracks[i].id+'">Play</a></li>';
+          var title = tracks[i].title;
+          var likes_cnt = tracks[i].favoritings_count;
+          var id = tracks[i].id;
+          var created = tracks[i].created_at;
+
+          var li = '<li>'+title+'(likes '+likes_cnt+') '+created+'&nbsp;<a href="#'+id+'">Play</a></li>';
           $('#search_result').find('ul').append(li);
         };
         $('#search_result').append('</ul>');
